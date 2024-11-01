2165d0fb2864af1e5644599408a0066e8918b299
김가현 <rkgus0324@gmail.com>
2024-11-01 18:27:28 +0900

    editpage.js 서버 임시 저장

--- 민정원/js/editpage.js
+++ 민정원/js/editpage.js
@@ -1,4 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+// editpage.js
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('')
+    const post = JSON.parse(localStorage.getItem('selectedPost'));
+
+    if (post) {
+        // 이미지 및 텍스트 필드에 값 설정
+        document.querySelector('.image').src = post.image;
+        document.querySelector('.pork').textContent = post.tag;
+        document.querySelector('.food_name').textContent = post.name;
+        document.querySelector('.explanation').innerHTML = `${post.info},<br><span class="food_name">${post.name}</span>`;
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
     const savedText = localStorage.getItem('savedText');
     if (savedText) {
         document.getElementById('text_area').value = savedText;
